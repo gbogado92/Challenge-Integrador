dd48dfce489320d97949d6a60cd67b9b0806505d
MigfredPerdomo <96066822+Branko307@users.noreply.github.com>
2023-12-20 11:30:26 -0300

    Update shopRoute.js
    F

--- src/routes/shopRoute.js
+++ src/routes/shopRoute.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const multer = require('multer')
 const shopController = require('./../controllers/shopController.js')
 const router = express.Router()
 
+const config = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, path.resolve(__dirname, '../../public/img')),
+    filename: (req, file, cb) => cb(null, Date.now() + '_' + file.originalname)
+})
 
+const receptor = multer({config})
 
 router.get('/shop', shopController.shop)
-router.get('/shop/item:id', shopController.id)
+router.get('/shop/item', shopController.id)
 router.post('/shop/item/:id/add', shopController.postAdd)
 router.get('/shop/cart', shopController.cart)
 router.post('/shop/cart', shopController.postCart) //cart checkout
