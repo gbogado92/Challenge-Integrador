71a32b126a8ed6c5125d20f55fc132120897abd1
MigfredPerdomo <96066822+Branko307@users.noreply.github.com>
2023-12-20 11:29:41 -0300

    Update authRoute.js
    Fix

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,15 +1,20 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const validator = require('../mw/validator.js')
+const multer = require('multer')
+const {query, validation} = require('express-validator')
 const authController = require('./../controllers/authController.js')
 const router = express.Router()
 
-router.use(bodyParser.json())
+const config = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, path.resolve(__dirname, '../../public/img')),
+    filename: (req, file, cb) => cb(null, Date.now() + '_' + file.originalname)
+})
+
+const receptor = multer({config})
 
 router.get('/auth/login', authController.login)
 router.post('/auth/login', authController.postLogin)
 router.get('/auth/register', authController.register)
-router.post('/auth/register', validator.newRegister,authController.postRegister)
+router.post('/auth/register', authController.postRegister)
 router.get('/auth/logout', authController.logout)
 
 module.exports = router
