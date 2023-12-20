1002b5a27fa7e8b7f934853b9340ec2978eb8138
MigfredPerdomo <96066822+Branko307@users.noreply.github.com>
2023-12-20 11:28:35 -0300

    Update adminRoute.js
    cambio

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -1,16 +1,21 @@
 const express = require('express')
-const uploadFile = require('../mw/multer.js')
-// const {requireAdmin} = require('../mw/express-session.js')
+const multer = require('multer')
+const {query} = require('express-validator')
 const adminController = require('./../controllers/adminController.js')
 const router = express.Router()
 
+const config = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, path.resolve(__dirname, '../../public/img')),
+    filename: (req, file, cb) => cb(null, Date.now() + '_' + file.originalname)
+})
 
+const receptor = multer({config})
 
 router.get('/admin', adminController.admin)
 router.get('/admin/create', adminController.create)
-router.post('/admin/create', uploadFile.fields([{ name: 'image_front', maxCount: 1 }, { name: 'image_back', maxCount: 1 }]), adminController.postCreate)
+router.post('/admin/create', adminController.postCreate)
 router.get('/admin/edit', adminController.edit)
-router.post('/admin/edit:id', uploadFile.fields([{ name: 'image_front', maxCount: 1 }, { name: 'image_back', maxCount: 1 }]), adminController.postId)
+router.put('/admin/edit:id', adminController.putId)
 router.delete('/admin/delete/:id', adminController.deleteId)
 
 module.exports =router
