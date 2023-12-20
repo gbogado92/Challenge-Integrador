b24011f1ccfa14e44471a393f9811ccb0d9887fb
MigfredPerdomo <96066822+Branko307@users.noreply.github.com>
2023-12-20 11:31:48 -0300

    Update mainController.js
    F

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,5 +1,4 @@
-const { getAllFunkosFromDB } = require('../models/model.js')
-
+//const fs = require('fs')// Por si necesito un leer un FileSystem
 module.exports ={
 
     inicio: (req, res) => {
@@ -7,26 +6,13 @@ module.exports ={
                 <a href="/home"> Vamos a la página principal</a>`)
         },
 
-    home: async (req, res) => {
-        try {
-            const funkos = await getAllFunkosFromDB();
-            res.render('index.ejs',
-                {
-                    title: 'Home | Funkoshop',
-                    funkos
-                })
-        } catch (error) {
-
-        }
-    },
+    home: (req, res) => {
+        res.render('index')
+        },
 
     contact: (req, res) => {
-        res.render('contacto',
-            {
-                title: 'Contacto | Funkoshop',
-                content: 'Ruta para la vista de contacto'
-            })
-    },
+        res.render('contacto')
+        },
 
     about: (req, res) => {
         res.render('index')
@@ -37,4 +23,13 @@ module.exports ={
         },
 
 
+    // postHome: function(req, res){
+    //     console.log(req.body) //repuestas del formulario
+    //     res.send(`<h1> Se procesó el formulario con el dato ${req.body.nombre} </h1>
+    //             <a href="/home"> Ir a la pagina principal</a>`)
+    //      },
+
+    // putHome:(req, res) => {
+    //     res.send('<h1>usamos method override</h1>')
+    // },
 }
