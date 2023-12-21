c2cea81659518d619751fd0c1ca4acb40846a4e8
Gabriel <gabrielgbogado92@gmail.com>
2023-12-21 16:19:23 -0300

    Agregar configuración de conexión a la base de datos local

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ app.use((req, res, next) => {
   res.status(404).render(`404`);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3306;
 
 app.listen(port, () =>
-  console.log("El server ésta funcionando en localhost:${port}")
+  console.log(`El server ésta funcionando en localhost: ${port}`)
 );
