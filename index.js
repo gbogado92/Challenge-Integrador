76b971bd08e1f31d67b80bc69636c76ed2cace34
Gabriel <gabrielgbogado92@gmail.com>
2023-12-21 19:21:56 -0300

    Modificación de codigo

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   res.status(404).render(`404`);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 33306;
 
 app.listen(port, () =>
   console.log(`El server ésta funcionando en localhost: ${port}`)
