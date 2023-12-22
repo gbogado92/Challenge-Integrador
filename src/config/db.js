9085731e20a4d75de50f0d15105b2af959a3ef72
Gabriel <gabriel@gmail.com>
2023-12-21 21:58:36 -0300

    fix envs

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 const { createPool } = require("mysql2/promise");
 
-const pool = createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
+console.log({
+  host: process.env.HOSTDB,
+  user: process.env.USERDB,
   password: process.env.DBPASS,
   database: process.env.DB,
   waitForConnections: true,
@@ -12,13 +12,27 @@ const pool = createPool({
   queueLimit: 0,
   port: process.env.DBPORT,
 });
-pool
-  .getConnection()
+
+const pool = createPool({
+  host: process.env.HOSTDB,  // Corregido de HOST a HOSTDB
+  user: process.env.USERDB,
+  password: process.env.DBPASS,
+  database: process.env.DB,
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+  port: parseInt(process.env.DBPORT, 10),  // Convertir a número
+});
+
+pool.getConnection()
   .then((connection) => {
     console.log("Connected to MySQL database");
     connection.release();
   })
   .catch((err) => {
-    console.log("Error connecting to MySQL:", err);
+    console.error("Error connecting to MySQL:", err);
+    process.exit(1);
   });
+
 module.exports = pool;
+
